791dc6b77931e5897708af21195560884e074c61
dgpecurso04 <39627421+dgpecurso04@users.noreply.github.com>
2018-06-15 18:02:16 -0700

    Test descomentado

--- src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
+++ src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -10,23 +10,13 @@ import static mx.unam.dgpe.sample.controller.RestUtil.*;
 public class TestMyController extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(TestMyController.class);
    
-//    @Test
+    @Test
     public void ok() throws Exception {
         //String result = sendGet("https://www.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
-        String result = sendGet("http://0.0.0.0:8080/api/tercero?mode=gusgus");
-        assertTrue("Este es un mensaje", result.length() >1);
-	logger.info(result);
 
-
-        result = sendGet("http://0.0.0.0:8080/api/tercero?mode=gusgus2");
+	String result = sendGet("http://0.0.0.0:8080/api/factorial?fac=5");
         assertTrue("Este es un mensaje", result.length() >1);
         logger.info(result);
-
-        result = sendGet("http://0.0.0.0:8080/api/tercero?mode=abcabc");
-        assertTrue("Este es un mensaje", result.length() >1);
-        logger.info(result);
-
-
 
     }
 
